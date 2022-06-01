f36b0f78792326958748f758eb0f97ea4790cfa7
Kazu <kazuokura88@gmail.com>
2022-06-01 11:13:39 -0700

    phase3

--- app.js
+++ app.js
@@ -40,15 +40,24 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', {
+    title: 'Home',
+    history: req.session.history,
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  res.render('about', {
+    title: 'About',
+    history: req.session.history,
+  });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', {
+    title: 'Contact',
+    history: req.session.history,
+  });
 });
 
 const port = 8080;
