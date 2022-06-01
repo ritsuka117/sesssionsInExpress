a99695d677d2db806d6185830e91c7a99e6bb58c
Kazu <kazuokura88@gmail.com>
2022-06-01 10:37:21 -0700

    phase1

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(session({
+  secret: 'a5d63fc5-17a5-459c-b3ba-6d81792158fc',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
